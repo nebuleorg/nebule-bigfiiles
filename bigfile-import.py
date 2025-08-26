4c042ac1b4248808008f484fad574973a6940f28
nebuleorg <deisto@nebule.org>
2025-08-26 11:30:26 +0200

    fix

---
+++ bigfile-import.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Special programme to import very fig files onto nebule systems..
+    Author: projet nebule
+    Version: 020250826
+    Licence: GNU GPL 2010-2025
+    Website: www.nebule.org
+"""
+
+import os
+import sys
+import time
+from datetime import datetime
+
+from nebule import nod_check_nid, blk_generate, blk_sign
